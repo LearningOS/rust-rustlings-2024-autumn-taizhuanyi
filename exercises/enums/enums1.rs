9853e90e86f3d68cd9c520ae8afe833cfe52b256
taizhuanyi <1825167564@qq.com>
2024-10-16 05:43:28 +0000

    solve structs3

--- exercises/enums/enums1.rs
+++ exercises/enums/enums1.rs
@@ -2,16 +2,17 @@
 //
 // No hints this time! ;)
 
-// I AM NOT DONE
-
-#[derive(Debug)]
-enum Message {
-    // TODO: define a few types of messages as used below
-}
-
-fn main() {
-    println!("{:?}", Message::Quit);
-    println!("{:?}", Message::Echo);
-    println!("{:?}", Message::Move);
-    println!("{:?}", Message::ChangeColor);
+#[derive(Debug)]  
+enum Message {  
+    Quit,  
+    Echo,  
+    Move,  
+    ChangeColor,  
+}  
+  
+fn main() {  
+    println!("{:?}", Message::Quit);  
+    println!("{:?}", Message::Echo);  
+    println!("{:?}", Message::Move);  
+    println!("{:?}", Message::ChangeColor);  
 }
--- exercises/enums/enums2.rs
+++ exercises/enums/enums2.rs
@@ -2,29 +2,33 @@
 //
 // Execute `rustlings hint enums2` or use the `hint` watch subcommand for a
 // hint.
-
-// I AM NOT DONE
-
-#[derive(Debug)]
-enum Message {
-    // TODO: define the different variants used below
-}
-
-impl Message {
-    fn call(&self) {
-        println!("{:?}", self);
-    }
-}
-
-fn main() {
-    let messages = [
-        Message::Move { x: 10, y: 30 },
-        Message::Echo(String::from("hello world")),
-        Message::ChangeColor(200, 255, 255),
-        Message::Quit,
-    ];
-
-    for message in &messages {
-        message.call();
-    }
+#[derive(Debug)]  
+enum Message {  
+    // 表示移动的消息，带有x和y坐标  
+    Move { x: i32, y: i32 },  
+    // 表示回显的消息，带有字符串数据  
+    Echo(String),  
+    // 表示更改颜色的消息，带有RGB值  
+    ChangeColor(i32, i32, i32),  
+    // 表示退出的消息，不带有任何数据  
+    Quit,  
+}  
+  
+impl Message {  
+    fn call(&self) {  
+        println!("{:?}", self);  
+    }  
+}  
+  
+fn main() {  
+    let messages = [  
+        Message::Move { x: 10, y: 30 },  
+        Message::Echo(String::from("hello world")),  
+        Message::ChangeColor(200, 255, 255),  
+        Message::Quit,  
+    ];  
+  
+    for message in &messages {  
+        message.call();  
+    }  
 }
